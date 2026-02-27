35e68a657256b2561252d00ba26ec28bd8ba193e
Vinicius Garcia <vgarcia@ciandt.com>
2026-02-27 18:05:42 -0300

    add fastfy api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import "dotenv/config";
+
+import Fastify from "fastify";
+
+// Instanciate the framework
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
